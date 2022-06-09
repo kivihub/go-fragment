190f25d4eae98b8f46d2912c3b8c319ff87fc75a
wangqiwei <wangqiwei@jd.com>
2022-06-10 01:13:03 +0800

    update: comment

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module go-fragment
 
 go 1.18
 
+require github.com/kivihub/go-project v0.0.0-20220609171025-ddc571bdbc13
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	go_project "github.com/kivihub/go-project"
+	utils "github.com/kivihub/go-project/util"
 	"go-fragment/util"
 )
 
 
 func main() {
 	util.Init()
+	go_project.SayHello()
+
+	// import时指定的是目录，跟package没关系
+	utils.Echo("aa")
 }
